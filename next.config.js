cf5ee62f648080591a32021fbe9605a84e71e6df
Manohar <itsmanoharkumar@gmail.com>
2023-04-25 17:41:51 +0530

    Initial Project Setup for GG Th

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["gg-the-gaming-guide.azurewebsites.net"],
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ types/types.ts
@@ -0,0 +1,114 @@
+export type Timestamp = {
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+};
+
+export type ShortcutAttributes = {
+  shortText: string;
+  windowsKeyCombo: string;
+  macKeyCombo: string;
+} & Timestamp;
+
+export type Shortcut = {
+  id: number;
+  attributes: ShortcutAttributes;
+};
+
+export type ShortcutCategoryAttributes = {
+  name: string;
+  shortcuts?: {
+    data: Array<Shortcut>;
+  };
+} & Timestamp;
+
+export type ShortcutCategory = {
+  id: number;
+  attributes: ShortcutCategoryAttributes;
+};
+
+export const enum OPERATING_SYSTEM {
+  MAC = "mac",
+  WINDOWS = "windows",
+}
+
+export type ProductAttributes = {
+  name: string;
+  logo: {
+    data: ImageData;
+  }
+} & Timestamp;
+
+export type Product = {
+  id: number;
+  attributes: ProductAttributes;
+};
+
+export type ImageData = {
+  id: number;
+  attributes: ImageAttributes;
+}
+
+export type ImageAttributes ={
+  name: string;
+  alternativeText: null | string;
+  caption: null | string;
+  width: number;
+  height: number;
+  formats: {
+    large: {
+      ext: string;
+      url: string;
+      hash: string;
+      mime: string;
+      name: string;
+      path: null | string;
+      size: number;
+      width: number;
+      height: number;
+    };
+    small: {
+      ext: string;
+      url: string;
+      hash: string;
+      mime: string;
+      name: string;
+      path: null | string;
+      size: number;
+      width: number;
+      height: number;
+    };
+    medium: {
+      ext: string;
+      url: string;
+      hash: string;
+      mime: string;
+      name: string;
+      path: null | string;
+      size: number;
+      width: number;
+      height: number;
+    };
+    thumbnail: {
+      ext: string;
+      url: string;
+      hash: string;
+      mime: string;
+      name: string;
+      path: null | string;
+      size: number;
+      width: number;
+      height: number;
+    };
+  };
+  hash: string;
+  ext: string;
+  mime: string;
+  size: number;
+  url: string;
+  previewUrl: null | string;
+  provider: string;
+  provider_metadata: null | object;
+  createdAt: string;
+  updatedAt: string;
+}
