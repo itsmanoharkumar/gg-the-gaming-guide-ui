53da1e27845164f92e4dcddce0f85a52f2b54a9f
Manohar <itsmanoharkumar@gmail.com>
2023-05-11 23:12:44 +0530

    We've known each other for so long

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const withPWA = require("next-pwa")({
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["gg-the-gaming-guide-web-app.azurewebsites.net/api", "localhost"],
+    domains: ["gg-the-gaming-guide-web-app.azurewebsites.net", "localhost"],
   },
 };
 
